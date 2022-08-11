cf01c31eb03dd046c2a2e24b629812f398043939
Luke Boniface <luke.boniface@wooga.net>
2022-08-11 16:40:44 +0200

    Initial commit

--- keyboards/splitkb/kyria/rev2/config.h
+++ keyboards/splitkb/kyria/rev2/config.h
@@ -17,6 +17,11 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
+#define NO_ACTION_LAYER
+#define DISABLE_LEADER
+#define NO_ACTION_MACRO
+#define NO_ACTION_FUNCTION
+
 /* USB Device descriptor parameter */
 #define VENDOR_ID    0x8D1D
 #define PRODUCT_ID   0x9D9D
--- keyboards/splitkb/kyria/rev2/rules.mk
+++ keyboards/splitkb/kyria/rev2/rules.mk
@@ -1,8 +1,15 @@
-OLED_ENABLE = yes
-OLED_DRIVER = SSD1306   # Enables the use of OLED displays
-ENCODER_ENABLE = yes       # Enables the use of one or more encoders
-RGBLIGHT_ENABLE = yes      # Enable keyboard RGB underglow
+EXTRAFLAGS += -flto
+OLED_ENABLE = no
+# OLED_DRIVER = SSD1306   # Enables the use of OLED displays
+ENCODER_ENABLE = no       # Enables the use of one or more encoders
+RGBLIGHT_ENABLE = no    # Enable keyboard RGB underglow
 RGB_MATRIX_ENABLE = no     # Enable keyboard RGB matrix (do not use together with RGBLIGHT_ENABLE)
-RGB_MATRIX_DRIVER = WS2812 # RGB matrix driver support
+# RGB_MATRIX_DRIVER = WS2812 # RGB matrix driver support
 
-RGB_MATRIX_SUPPORTED = yes
+RGB_MATRIX_SUPPORTED = no
+CONSOLE_ENABLE = yes
+MOUSEKEY_ENABLE = no
+EXTRAKEY_ENABLE = no
+COMMAND_ENABLE = no
+UNICODE_ENABLE = no
+SWAP_HANDS_ENABLE = no
