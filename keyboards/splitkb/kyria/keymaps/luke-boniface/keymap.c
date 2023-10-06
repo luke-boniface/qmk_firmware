d36c3fc6e3b7e27cd9f3632d43a9c118aa9a07dd
Luke Boniface <luke.boniface@wooga.net>
2023-10-06 10:59:34 +0200

    Update function keys

--- keyboards/splitkb/kyria/keymaps/luke-boniface/keymap.c
+++ keyboards/splitkb/kyria/keymaps/luke-boniface/keymap.c
@@ -11,15 +11,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[5] = LAYOUT(KC_NO, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_NO, KC_NO, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, OSM(MOD_LCTL|MOD_LSFT|MOD_LGUI), KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, OSM(MOD_LSFT|MOD_LGUI), KC_NO, KC_NO, KC_F11, KC_F12, KC_NO, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS)
 };
 
-//LT(4,KC_ESC)
-
 layer_state_t layer_state_set_user(layer_state_t state) {
     switch (get_highest_layer(state)) {
     case 4:
-        SEND_STRING(SS_TAP(X_F14));
+        SEND_STRING(SS_TAP(X_F17));
         break;
     case 0:
-        SEND_STRING(SS_TAP(X_F15));
+        SEND_STRING(SS_TAP(X_F16));
     break;
     }
   return state;
